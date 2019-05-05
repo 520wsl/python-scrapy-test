8ce1679f361b376140c00faba11d2680b6d9a52e
520wsl <395548460@qq.com>
2019-05-05 17:36:46 +0800

    糗事百科

--- qsbk/qsbk/pipelines.py
+++ qsbk/qsbk/pipelines.py
@@ -6,20 +6,57 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
 
+# class QsbkPipeline(object):
+#     def __init__(self):
+#         self.fp = open('dunzi.json', 'w', encoding='utf-8')
+#
+#     def open_spider(self, spider):
+#         print('爬虫开始。。。。。。。。。。。。')
+#
+#     def process_item(self, item, spider):
+#         item_json = json.dumps(dict(item),ensure_ascii=False)
+#         self.fp.write(item_json + '\n')
+#         return item
+#
+#     def close_spider(self, spider):
+#         self.fp.close()
+#         print('爬虫结束。。。。。。。。。')
+
+from scrapy.exporters import JsonItemExporter
+
+# class QsbkPipeline(object):
+#     def __init__(self):
+#         self.fp = open('dunzi.json', 'wb')
+#         self.exporter = JsonItemExporter(self.fp, ensure_ascii=False, encoding='utf-8')
+#         self.exporter.start_exporting()
+#
+#     def open_spider(self, spider):
+#         print('爬虫开始。。。。。。。。。。。。')
+#
+#     def process_item(self, item, spider):
+#         self.exporter.export_item(item=item)
+#         return item
+#
+#     def close_spider(self, spider):
+#         self.exporter.finish_exporting()
+#         self.fp.close()
+#         print('爬虫结束。。。。。。。。。')
+
+from scrapy.exporters import JsonLinesItemExporter
+
 
 class QsbkPipeline(object):
     def __init__(self):
-        self.fp = open('dunzi.json', 'w', encoding='utf-8')
+        self.fp = open('dunzi.json', 'wb')
+        self.exporter = JsonLinesItemExporter(self.fp, ensure_ascii=False, encoding='utf-8')
 
     def open_spider(self, spider):
         print('爬虫开始。。。。。。。。。。。。')
 
     def process_item(self, item, spider):
-        item_json = json.dumps(dict(item),ensure_ascii=False)
-        self.fp.write(item_json + '\n')
+        self.exporter.export_item(item=item)
         return item
 
     def close_spider(self, spider):
         self.fp.close()
         print('爬虫结束。。。。。。。。。')
-
