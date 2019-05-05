145ef1b209cd3f58facd97394a6757fc2f089a1c
520wsl <395548460@qq.com>
2019-05-05 16:54:18 +0800

    糗事百科

--- qsbk/qsbk/spiders/qiushibaike.py
+++ qsbk/qsbk/spiders/qiushibaike.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.selector.unified import SelectorList
 
 class QiushibaikeSpider(scrapy.Spider):
     name = 'qiushibaike'
     allowed_domains = ['qiushibaike.com']
-    start_urls = ['http://www.qiushibaike.com/']
+    start_urls = ['https://www.qiushibaike.com/text/page/1/']
 
     def parse(self, response):
+        duanzidivs = response.xpath("//div[@id='content-left']/div")
+        self.log(type(duanzidivs))
+        self.log(duanzidivs)
+
+        for duanzidiv in duanzidivs:
+            self.log(type(duanzidiv))
+            author = duanzidiv.xpath('.//h2/text()').get().strip()
+            self.log(author)
+            content = duanzidiv.xpath('.//div[@class="content"]//text()').getall()
+            content = " ".join(content).strip()
+            self.log(content)
         pass
