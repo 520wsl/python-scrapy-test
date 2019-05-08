4013b6419109918e9188ce2a5ec5b77c6da3ed9d
520wsl <395548460@qq.com>
2019-05-08 22:10:18 +0800

    汽车之家图片

--- douban_login/douban_login/spiders/douban_login.py
+++ douban_login/douban_login/spiders/douban_login.py
@@ -6,49 +6,48 @@ import scrapy
 
 
 class DoubanSpider(scrapy.Spider):
-    name = 'douban'
+    name = 'douban_login'
     allowed_domains = ['douban.com']
-    start_urls = ['https://accounts.douban.com/login']
-    login_url = 'https://accounts.douban.com/login'
+    start_urls = ['https://accounts.douban.com/passport/login']
+    login_url = 'https://accounts.douban.com/j/mobile/login/basic'
     profile_url = "https://www.douban.com/people/196062677/"
     editsignature_url = "https://www.douban.com/j/people/196062677/edit_signature"
 
     def parse(self, response):
         formdata = {
-            'source': 'None',
-            'redir': 'https://www.douban.com/',
-            'form_email': '970138074@qq.com',
-            'form_password': 'pythonspider',
-            'remember': 'on',
-            'login': '登录'
+            'ck': '',
+            'name': '13625719920',
+            'password': 'maddragon016613.',
+            'remember': 'false',
+            'ticket': ''
         }
-        captcha_url = response.css('img#captcha_image::attr(src)').get()
-        if captcha_url:
-            captcha = self.regonize_captcha(captcha_url)
-            formdata['captcha-solution'] = captcha
-            capthca_id = response.xpath("//input[@name='captcha-id']/@value").get()
-            formdata['captcha-id'] = capthca_id
+        # captcha_url = response.css('img#captcha_image::attr(src)').get()
+        # if captcha_url:
+        #     captcha = self.regonize_captcha(captcha_url)
+        #     formdata['captcha-solution'] = captcha
+        #     capthca_id = response.xpath("//input[@name='captcha-id']/@value").get()
+        #     formdata['captcha-id'] = capthca_id
         yield scrapy.FormRequest(url=self.login_url, formdata=formdata, callback=self.parse_after_login)
 
     def parse_after_login(self, response):
-        if response.url == 'https://www.douban.com/':
+        if response.url == self.login_url:
             yield scrapy.Request(self.profile_url, callback=self.parse_profile)
-            print('登录成功')
+            self.log('登录成功')
         else:
-            print('登录失败')
+            self.log('登录失败')
 
     def parse_profile(self, response):
-        print(response.url)
+        self.log(response.url)
         if response.url == self.profile_url:
-            print('进入到个人中心')
+            self.log('进入到个人中心')
             ck = response.xpath("//input[@name='ck']/@value").get()
             formdata = {
                 'ck': ck,
-                'signature': '我就是我，不一样的烟火~~'
+                'signature': '虽然年少轻狂，却只知道胜者为王'
             }
             yield scrapy.FormRequest(self.editsignature_url, formdata=formdata)
         else:
-            print('没有进入到个人中心')
+            self.log('没有进入到个人中心')
 
     def regonize_captcha(self, image_url):
         request.urlretrieve(image_url, 'captcha.png')
--- douban_login/start.py
+++ douban_login/start.py
@@ -1,3 +1,3 @@
 from scrapy import cmdline
 
-cmdline.execute(['scrapy', 'crawl', 'douban'])
+cmdline.execute(['scrapy', 'crawl', 'douban_login'])
